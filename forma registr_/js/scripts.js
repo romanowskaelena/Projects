45096523f1f5b3f02067308ed0740c69af0b659b
Elena Romanowska <148208289+romanowskaelena@users.noreply.github.com>
2024-02-22 20:07:37 +0100

    Add files via upload

---
+++ forma registr_/js/scripts.js
@@ -0,0 +1,9 @@
+// alert ("Hello")
+
+let userEmail = document.querySelector(".inpEmail");
+
+function signUp() {
+   let userUnput1 = userEmail.value;
+   console.log("your e-mail: " + userUnput1); 
+}
+
